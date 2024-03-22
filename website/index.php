587dbbc8a1d89b7b479c69f13bc5e81473653b5c
CruzM-224 <cruzmoreno022002@gmail.com>
2024-03-21 20:45:49 -0600

    Form check-in y check-out
    Se realizo el form del check-in y check-out, cantidad de adultos, niños y un boton de submit

--- website/index.php
+++ website/index.php
@@ -57,6 +57,33 @@
 		  -moz-appearance: textfield;
 		}
 
+		.custom-bg{
+			background-color: #2ec1ac;
+		}
+
+		.custom-bg:hover{
+			background-color: #279e8c;
+		}
+
+		.availability-form{
+			margin-top: -75px;
+			z-index: 2;
+			position: relative;
+		}
+
+		@media screen and (max-width: 575px) {
+			.availability-form{
+				margin-top: 0px;
+				padding: 0 35px;
+			}
+		}
+
+		@media (min-width: 992px) and (max-width: 1199.98px){
+			.boton{	
+				margin-left: -1rem;	
+			}
+		}
+
 	</style>
 </head>
 <body class="bg-light">
@@ -197,6 +224,8 @@
 	  </div>
 	</div>
 
+
+	<!-- Carousel -->
 	<!-- Swiper -->
 	<div class="container-fluid px-lg-4 mt-4">
 		<div class="swiper swiper-container">
@@ -222,6 +251,51 @@
 		    </div>
   		</div>
 	</div>
+
+	<!-- Check availability form -->\
+	<div class="container availability-form">
+		<div class="row">
+			<div class="col-lg-12 bg-white shadow p-4 rounded">
+				<h5 class="mb-4">Check Booking Availability</h5>
+				<form>
+					<div class="row align-items-end">
+						<div class="col-lg-3 mb-3">
+							<label class="form-label" style="font-weight: 500;">Check-in</label>
+					    	<input type="date" class="form-control shadow-none">
+						</div>
+						<div class="col-lg-3 mb-3">
+							<label class="form-label" style="font-weight: 500;">Check-out</label>
+					    	<input type="date" class="form-control shadow-none">
+						</div>
+						<div class="col-lg-3 mb-3">
+							<label class="form-label" style="font-weight: 500;">Adult</label>
+					    	<select class="form-select shadow-none">
+							  <option selected>Open this select menu</option>
+							  <option value="1">One</option>
+							  <option value="2">Two</option>
+							  <option value="3">Three</option>
+							</select>
+						</div>
+						<div class="col-lg-2 mb-3">
+							<label class="form-label" style="font-weight: 500;">Children</label>
+					    	<select class="form-select shadow-none">
+							  <option selected>Open this select menu</option>
+							  <option value="1">One</option>
+							  <option value="2">Two</option>
+							  <option value="3">Three</option>
+							</select>
+						</div>
+						<div class="boton col-lg-1 mb-lg-3 mt-2">
+							<button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<br><br><br>
+	<br><br><br>
 
 	<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
