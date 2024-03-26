cc5acef3c79b176b7d007ad64de33667582ceef9
CruzM-224 <cruzmoreno022002@gmail.com>
2024-03-26 16:07:19 -0600

    Apartado de cuartos y css aparte
    Se hicieron las tarjetas de los cuartos y un css en otro archivo distinto

--- website/index.php
+++ website/index.php
@@ -8,63 +8,9 @@
 	<link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/common.css">
 
 	<style type="text/css">
-		.merienda-<uniquifier> {
-		  font-family: "Merienda", cursive;
-		  font-optical-sizing: auto;
-		  font-weight: <weight>;
-		  font-style: normal;
-		}
-
-		.poppins-regular {
-		  font-family: "Poppins", sans-serif;
-		  font-weight: 400;
-		  font-style: normal;
-		}
-
-		.poppins-medium {
-		  font-family: "Poppins", sans-serif;
-		  font-weight: 500;
-		  font-style: normal;
-		}
-
-		.poppins-semibold {
-		  font-family: "Poppins", sans-serif;
-		  font-weight: 600;
-		  font-style: normal;
-		}
-
-		*{
-			font-family: "Poppins", sans-serif;
-		}
-
-		.h-font{
-			font-family: "Merienda", cursive;
-		}
-
-		/* Ocultar las flechas del input number */
-
-		/* Chrome, Safari, Edge, Opera */
-		input::-webkit-outer-spin-button,
-		input::-webkit-inner-spin-button {
-		  -webkit-appearance: none;
-		  margin: 0;
-		}
-
-		/* Firefox */
-		input[type=number] {
-		  -moz-appearance: textfield;
-		}
-
-		.custom-bg{
-			background-color: #2ec1ac;
-		}
-
-		.custom-bg:hover{
-			background-color: #279e8c;
-		}
-
 		.availability-form{
 			margin-top: -75px;
 			z-index: 2;
@@ -83,7 +29,6 @@
 				margin-left: -1rem;	
 			}
 		}
-
 	</style>
 </head>
 <body class="bg-light">
@@ -293,6 +238,174 @@
 			</div>
 		</div>
 	</div>
+
+
+	<!-- Our rooms -->
+	<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR ROOMS</h2>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-6 my-3">
+				<div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+				  <img src="images/rooms/1.jpg" class="card-img-top">
+				  <div class="card-body">
+				    <h5>Simple Room Name</h5>
+				    <h6 class="mb-4">$200 per night</h6>
+				    <div class="features mb-4">
+				    	<h6 class="mb-1">Features</h6>
+				    	<span class="badge rounded-pill text-bg-light text-wrap">
+				    		2 Rooms
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		1 Bathroom
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		1 Balcony
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		3 Sofa
+						</span>
+				    </div>
+				    <div class="facilities mb-4">
+				    	<h6 class="mb-1">Facilities</h6>
+				    	<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Wifi
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Television
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		AC
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Room heater
+						</span>
+				    </div>
+				    <div class="rating mb-4">
+				    	<h6 class="mb-1">Rating</h6>
+				    	<span class="badge rounded-pill bg-light">
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+				    	</span>
+				    </div>
+				    <div class="d-flex justify-content-evenly mb-2">
+				    	<a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+				    	<a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 my-3">
+				<div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+				  <img src="images/rooms/1.jpg" class="card-img-top">
+				  <div class="card-body">
+				    <h5>Simple Room Name</h5>
+				    <h6 class="mb-4">$200 per night</h6>
+				    <div class="features mb-4">
+				    	<h6 class="mb-1">Features</h6>
+				    	<span class="badge rounded-pill text-bg-light text-wrap">
+				    		2 Rooms
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		1 Bathroom
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		1 Balcony
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		3 Sofa
+						</span>
+				    </div>
+				    <div class="facilities mb-4">
+				    	<h6 class="mb-1">Facilities</h6>
+				    	<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Wifi
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Television
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		AC
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Room heater
+						</span>
+				    </div>
+				    <div class="rating mb-4">
+				    	<h6 class="mb-1">Rating</h6>
+				    	<span class="badge rounded-pill bg-light">
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+				    	</span>
+				    </div>
+				    <div class="d-flex justify-content-evenly mb-2">
+				    	<a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+				    	<a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 my-3">
+				<div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
+				  <img src="images/rooms/1.jpg" class="card-img-top">
+				  <div class="card-body">
+				    <h5>Simple Room Name</h5>
+				    <h6 class="mb-4">$200 per night</h6>
+				    <div class="features mb-4">
+				    	<h6 class="mb-1">Features</h6>
+				    	<span class="badge rounded-pill text-bg-light text-wrap">
+				    		2 Rooms
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		1 Bathroom
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		1 Balcony
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		3 Sofa
+						</span>
+				    </div>
+				    <div class="facilities mb-4">
+				    	<h6 class="mb-1">Facilities</h6>
+				    	<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Wifi
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Television
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		AC
+						</span>
+						<span class="badge rounded-pill text-bg-light text-wrap">
+				    		Room heater
+						</span>
+				    </div>
+				    <div class="rating mb-4">
+				    	<h6 class="mb-1">Rating</h6>
+				    	<span class="badge rounded-pill bg-light">
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+					    	<i class="bi bi-star-fill text-warning"></i>
+				    	</span>
+				    </div>
+				    <div class="d-flex justify-content-evenly mb-2">
+				    	<a href="#" class="btn btn-sm text-white custom-bg shadow-none">Book Now</a>
+				    	<a href="#" class="btn btn-sm btn-outline-dark shadow-none">More Details</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-lg-12 text-center mt-5">
+				<a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms >>></a>
+			</div>
+		</div>
+	</div>
+
 
 	<br><br><br>
 	<br><br><br>
