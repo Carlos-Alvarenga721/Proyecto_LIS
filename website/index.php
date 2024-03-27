d4729e3d4f8c6d1b359fe79b79399fb3898b1340
CruzM-224 <cruzmoreno022002@gmail.com>
2024-03-26 18:25:11 -0600

    Facilities, comentarios y mapa junto con footer

--- website/index.php
+++ website/index.php
@@ -406,9 +406,185 @@
 		</div>
 	</div>
 
+	<!-- Our facilites -->
+	<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+	<div class="container">
+		<div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+			<div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+				<img src="images/facilities/wifi.svg" width="80px">
+				<h5 class="mt-3">Wifi</h5>
+			</div>
+			<div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+				<img src="images/facilities/wifi.svg" width="80px">
+				<h5 class="mt-3">Wifi</h5>
+			</div>
+			<div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+				<img src="images/facilities/wifi.svg" width="80px">
+				<h5 class="mt-3">Wifi</h5>
+			</div>
+			<div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+				<img src="images/facilities/wifi.svg" width="80px">
+				<h5 class="mt-3">Wifi</h5>
+			</div>
+			<div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+				<img src="images/facilities/wifi.svg" width="80px">
+				<h5 class="mt-3">Wifi</h5>
+			</div>
+			<div class="col-lg-12 text-center mt-5">
+				<a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+			</div>
+		</div>
+	</div>
 
-	<br><br><br>
-	<br><br><br>
+	<!-- Testimonials -->
+	<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
+	<div class="container mt-5">
+		<!-- Swiper -->
+		<div class="swiper swiper-testimonials">
+		<div class="swiper-wrapper mb-5">
+
+	  		<div class="swiper-slide bg-white p-4">
+		    	<div class="profile d-flex align-items-center mb-3">
+		    		<img src="images/facilities/wifi.svg" width="30px">
+		    		<h6 class="m-0 ms-2">Random User 1</h6>
+		    	</div>
+		    	<p>
+		    		Bla Bla Bla Bla.
+		    		Bla Bla Bla Bla Bla.
+		    		Bla Bla.
+		    	</p>
+		    	<div class="rating">
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    	</div>
+		  	</div>
+		  	<div class="swiper-slide bg-white p-4">
+		    	<div class="profile d-flex align-items-center mb-3">
+		    		<img src="images/facilities/wifi.svg" width="30px">
+		    		<h6 class="m-0 ms-2">Random User 1</h6>
+		    	</div>
+		    	<p>
+		    		Bla Bla Bla Bla.
+		    		Bla Bla Bla Bla Bla.
+		    		Bla Bla.
+		    	</p>
+		    	<div class="rating">
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    	</div>
+		  	</div>
+		  	<div class="swiper-slide bg-white p-4">
+		    	<div class="profile d-flex align-items-center mb-3">
+		    		<img src="images/facilities/wifi.svg" width="30px">
+		    		<h6 class="m-0 ms-2">Random User 1</h6>
+		    	</div>
+		    	<p>
+		    		Bla Bla Bla Bla.
+		    		Bla Bla Bla Bla Bla.
+		    		Bla Bla.
+		    	</p>
+		    	<div class="rating">
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    		<i class="bi bi-star-fill text-warning"></i>
+		    	</div>
+		  	</div>
+
+		</div>
+		<div class="swiper-pagination"></div>
+		</div>
+		<div class="col-lg-12 text-center mt-5">
+			<a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+		</div>
+	</div>
+
+	<!-- Reach Us -->
+	<h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+				<iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15528.428566436925!2d-89.00638889999999!3d13.343610850000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7cba1049086983%3A0xda43ef28ca8faef3!2sPlaya%20Costa%20del%20Sol!5e0!3m2!1ses-419!2ssv!4v1711497030150!5m2!1ses-419!2ssv"loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+			<div class="col-lg-4 col-md-4">
+				<div class="bg-white p-4 rounded mb-4">
+					<h5>Call us</h5>
+					<a href="tel: +503" class="d-inline-block mb-2 text-decoration-none text-dark">
+						<i class="bi bi-telephone-fill"></i> Numero de telefono
+					</a>
+					<br>
+					<a href="tel: +503" class="d-inline-block text-decoration-none text-dark">
+						<i class="bi bi-telephone-fill"></i> Numero de telefono
+					</a>
+				</div>
+				<div class="bg-white p-4 rounded mb-4">
+					<h5>Follow us</h5>
+					<a href="#" class="d-inline-block mb-3">
+						<span class="badge bg-light text-dark fs-6 p-2">
+							<i class="bi bi-twitter-x me-1"></i> Twitter
+						</span>
+					</a>
+					<br>
+					<a href="#" class="d-inline-block mb-3">
+						<span class="badge bg-light text-dark fs-6 p-2">
+							<i class="bi bi-facebook me-1"></i> Facebook
+						</span>
+					</a>
+					<br>
+					<a href="#" class="d-inline-block">
+						<span class="badge bg-light text-dark fs-6 p-2">
+							<i class="bi bi-instagram me-1"></i> Instagram
+						</span>
+					</a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<div class="container-fluid bg-white mt-5">
+		<div class="row">
+			<div class="col-lg-4 p-4">
+				<h3 class="h-font fw-bold fs-3 mb-2">HOTEL</h3>
+				<p>
+					Bla bla bla bla bla.
+					Bla bla bla bla.
+					Bla bla bla bla Bla bla bla bla.
+					Bla bla bla bla. Bla bla bla bla. Bla bla bla bla.
+				</p>
+			</div>
+			<div class="col-lg-4 p-4">
+				<h5 class="mb-3">Links</h5>
+				<a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+				<a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+				<a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+				<a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a> <br>
+				<a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+			</div>
+			<div class="col-lg-4 p-4">
+				<h5 class="mb-3">Follow us</h5>
+				<a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+					<i class="bi bi-twitter-x me-1"></i> Twitter
+				</a>
+				<br>
+				<a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+					<i class="bi bi-facebook me-1"></i> Facebook
+				</a>
+				<br>
+				<a href="#" class="d-inline-block text-dark text-decoration-none">
+					<i class="bi bi-instagram me-1"></i> Instagram
+				</a>
+				<br>
+			</div>
+		</div>
+	</div>
+
+	<h6 class="text-center bg-dark text-white p-3 m-0">Hotel</h6>
 
 	<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
@@ -420,6 +596,39 @@
 	      autoplay: {
 	      	delay: 3500,
 	      	disableOnInteraction: false
+	      }
+	    });
+
+	    var swiper = new Swiper(".swiper-testimonials", {
+	      effect: "coverflow",
+	      grabCursor: true,
+	      centeredSlides: true,
+	      slidesPerView: "auto",
+	      slidesPerView: 3,
+	      loop: true,
+	      coverflowEffect: {
+	        rotate: 50,
+	        stretch: 0,
+	        depth: 100,
+	        modifier: 1,
+	        slideShadows: false,
+	      },
+	      pagination: {
+	        el: ".swiper-pagination",
+	      },
+	      breakpoints: {
+	      	320: {
+	      		slidesPerView: 1,
+	      	},
+	      	640: {
+	      		slidesPerView: 1,
+	      	},
+	      	768: {
+	      		slidesPerView: 2,
+	      	},
+	      	1024: {
+	      		slidesPerView: 3,
+	      	},
 	      }
 	    });
   	</script>
