7d1fa63cf490cd47e740aff6b815b539358cc316
Carlos Alvarenga <c.alvarenga.c7@gmail.com>
2024-03-29 11:43:51 -0600

    facilities

---
+++ website/facilities.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hotel - Facilities</title>	
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+	<?php require('inc/links.php');?>
+	<style>
+		.pop:hover{
+			border-top-color: var(--teal) !important;
+			transform: scale(1.03);
+			transition: all 0.3s ;
+		}
+	</style>
+</head>
+<body class="bg-light">
+	<!--Mando a llamar al archivo header-->
+	<?php require('inc/header.php');?>
+
+	<div class="my-5 px-4">
+		<h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+		<div class="h-line bg-dark"></div>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe velit tenetur fugit accusantium, nobis facere eligendi accusamus quaerat, <br> molestias, sunt exercitationem necessitatibus earum voluptatem voluptatum ducimus perferendis ea porro enim!
+		</p>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<div class="d-flex align-items-center mb-2">
+						<img src="images/facilities/wifi.svg" alt="wifi" width="40px">
+						<h5 class="m-0 ms-3">Wifi</h5>
+					</div>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem qui nisi vel reiciendis eos.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<div class="d-flex align-items-center mb-2">
+						<img src="images/facilities/wifi.svg" alt="wifi" width="40px">
+						<h5 class="m-0 ms-3">Wifi</h5>
+					</div>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem qui nisi vel reiciendis eos.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<div class="d-flex align-items-center mb-2">
+						<img src="images/facilities/wifi.svg" alt="wifi" width="40px">
+						<h5 class="m-0 ms-3">Wifi</h5>
+					</div>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem qui nisi vel reiciendis eos.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<div class="d-flex align-items-center mb-2">
+						<img src="images/facilities/wifi.svg" alt="wifi" width="40px">
+						<h5 class="m-0 ms-3">Wifi</h5>
+					</div>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem qui nisi vel reiciendis eos.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<div class="d-flex align-items-center mb-2">
+						<img src="images/facilities/wifi.svg" alt="wifi" width="40px">
+						<h5 class="m-0 ms-3">Wifi</h5>
+					</div>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem qui nisi vel reiciendis eos.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<div class="d-flex align-items-center mb-2">
+						<img src="images/facilities/wifi.svg" alt="wifi" width="40px">
+						<h5 class="m-0 ms-3">Wifi</h5>
+					</div>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem qui nisi vel reiciendis eos.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer -->
+	<?php require('inc/footer.php');?>
+
+	
+</body>
+</html>
